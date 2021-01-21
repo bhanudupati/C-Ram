3204dc38dd3128d56f35956d96fa4c9a90395eec
bhanudupati <76739440+bhanudupati@users.noreply.github.com>
2021-01-21 20:45:51 +0530

    Create SI with float pr17.c

---
+++ SI with float pr17.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int p,t,r;
+float SI;
+clrscr();
+printf("enter p");
+scanf("%d",&p);
+printf("enter t");
+scanf("%d",&t);
+printf("enter r");
+scanf("%d",&r);
+SI=(p*t*r)/100;
+printf("SI is = %f",SI);
+getch();
+}
