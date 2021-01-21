193fab5dc64e06494f10ba8c1b2f1e6d1dc95a60
bhanudupati <76739440+bhanudupati@users.noreply.github.com>
2021-01-21 20:50:21 +0530

    Create switch concept vowels pr19.c

---
+++ switch concept vowels pr19.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch='a';
+clrscr();
+switch (ch)
+{
+case 'a':
+printf("vowel");
+break;
+case 'e':
+printf("vowel");
+break;
+case 'i':
+printf("vowel");
+break;
+case 'o':
+printf("vowel");
+break;
+case 'u':
+printf("vowel");
+break;
+default:
+printf("consonant");
+}
+getch();
+}
