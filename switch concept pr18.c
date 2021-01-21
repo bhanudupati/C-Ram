98e192a4311fe299602fd12c48385aabeab7c178
bhanudupati <76739440+bhanudupati@users.noreply.github.com>
2021-01-21 20:48:37 +0530

    Create switch concept pr18.c

---
+++ switch concept pr18.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num=2;
+clrscr();
+switch(num)
+{
+case 1:
+printf("one");
+break;
+case 2:
+printf("two");
+break;
+case 3:
+printf("three");
+break;
+default:
+printf("invalid input");
+break;
+}
+getch();
+}
