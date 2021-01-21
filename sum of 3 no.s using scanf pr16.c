419e932e54403cc7ba4804757eab5543b78fa21f
bhanudupati <76739440+bhanudupati@users.noreply.github.com>
2021-01-21 20:43:11 +0530

    Create sum of 3 no.s using scanf pr16.c

---
+++ sum of 3 no.s using scanf pr16.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int s,a,b,c;
+clrscr();
+printf("enter a");
+scanf("%d",&a);
+printf("enter b");
+scanf("%d",&b);
+printf("enter c");
+scanf("%d",&c);
+s=a+b+c;
+printf("the sum is = %d",s);
+getch();
+}
