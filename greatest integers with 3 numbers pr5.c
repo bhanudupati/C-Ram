507068df6e6c08e0a9890e7645a28f2d454c72d6
bhanudupati <76739440+bhanudupati@users.noreply.github.com>
2021-01-21 18:03:26 +0530

    Create greatest integers with 3 numbers pr5.c

---
+++ greatest integers with 3 numbers pr5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=22;
+int b=78;
+int c=85;
+clrscr();
+if (a>b && a>c)
+printf("a is the greatest");
+else if (b>a && b>c)
+printf("b is the greatest");
+else
+printf("c is the greatest")
+getch();
+}
