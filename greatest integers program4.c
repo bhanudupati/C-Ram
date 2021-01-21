d2145f7d1a8f0354769a19920555ca535eea49b1
bhanudupati <76739440+bhanudupati@users.noreply.github.com>
2021-01-21 17:24:07 +0530

    Create greatest integers program4.c

---
+++ greatest integers program4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=12;
+int b=35;
+clrscr();
+if (a>b)
+printf("a is the greatest integer");
+else
+printf("b is the greatest integer");
+getch();
+}
