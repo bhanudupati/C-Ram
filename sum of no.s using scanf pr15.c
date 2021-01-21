b94f99735395688a577913bd746bff73cdb7e793
bhanudupati <76739440+bhanudupati@users.noreply.github.com>
2021-01-21 19:57:43 +0530

    Create sum of no.s using scanf pr15.c

---
+++ sum of no.s using scanf pr15.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num1,num2;
+clrscr();
+printf("Enter the value for num1");
+scanf("%d",&num1);
+printf("Enter the value for num2");
+scanf("%d",&num2);
+printf("Sum is %d",num1+num2);
+getch();
+}
