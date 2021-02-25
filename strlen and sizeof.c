289aa33da1d3f898a64ba84547ed546b9ab0fb66
bhanudupati <76739440+bhanudupati@users.noreply.github.com>
2021-02-25 20:49:45 +0530

    Update strlen and sizeof.c

--- strlen and sizeof.c
+++ strlen and sizeof.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char name[30]="Bhavana";
+clrscr();
+printf("length of my name is %d",strlen(name));
+printf("size of my name is %d",sizeof(name));
+getch();
+}
