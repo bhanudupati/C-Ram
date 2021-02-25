62c55dc7aad31045391e4c0e5d1a122cfcb6a2b4
bhanudupati <76739440+bhanudupati@users.noreply.github.com>
2021-02-25 21:10:39 +0530

    Update string compare.c

--- string compare.c
+++ string compare.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char name1[30]="Bhavana";
+char name2[30]="Bhavana123";
+clrscr();
+if(strcmp(name1,name2)==0)
+printf("name1 and name2 are equal");
+else
+printf("name1 and name2 are different");
+getch();
+}
