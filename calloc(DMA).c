58c500af31e4058b9d0acc79240c57fab0df4e6a
bhanudupati <76739440+bhanudupati@users.noreply.github.com>
2021-03-09 20:52:52 +0530

    Create calloc(DMA).c

---
+++ calloc(DMA).c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int *p;
+ int n,i;
+ n=5;
+ clrscr();
+ p=(int*)calloc(n*sizeof(int));
+ if (p==NULL)
+ {
+  printf("memory not allocated \n");
+  exit(0);
+ }
+ else
+ {
+  printf("memory is allocated");
+ }
+ for(i=0;i<n;i++)
+ {
+  p[i]=i+1;
+ }
+ for(i=0;i<n;i++)
+ {
+  printf("%d",p[i]);
+ }
+ getch();
+} 
