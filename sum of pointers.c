ce38f8518931af455fa93b557a56ce678aebf419
bhanudupati <76739440+bhanudupati@users.noreply.github.com>
2021-03-29 20:56:29 +0530

    Create sum of pointers.c

---
+++ sum of pointers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+{
+  int sum;
+  int a=10;
+  int b=20;
+  int *p;
+  p=&a;
+  int *q;
+  q=&b;
+  sum=a+b;
+  printf("the sum of a and b is %d",sum);
+  getch();
+}
